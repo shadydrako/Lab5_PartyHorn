836214614528f63647f317659ff7678f5cae7895
Ravi <rtapia@ucsd.edu>
2021-02-02 21:04:28 -0800

    horn now works

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,6 @@
  //main.js
-
- 
-function playSound(){
-    alert( "The button was pressed");
-    var audioElement = new Audio('assets\media\audio\air-horn.mp3');
-    audioElement.muted=false;
-    audioElement.play();
-}
-
-var btn = document.querySelector('button').addEventListener("click",playSound());
-
-
- 
+document.querySelector('button').addEventListener('click',function(){
+    let audio = document.getElementById("horn-sound");
+    audio.play();
+});
 // TODO
