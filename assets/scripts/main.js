5a1c91060cdc2165e438ed13a98b58ff72052fc2
Ravi <rtapia@ucsd.edu>
2021-02-02 23:08:47 -0800

    able to choose sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,22 @@
  //main.js
+
+let sounds = ["./assets/media/audio/air-horn.mp3","./assets/media/audio/car-horn.mp3","./assets/media/audio/party-horn.mp3"];
+
 document.querySelector('button').addEventListener('click',function(){
+    let s = 0;
+    var form = document.querySelector("form");
+    var radios = document.getElementsByName("radio-sound");
+    for(let i = 0, length = radios.length; i < length; i++){
+        if(radios[i].checked){
+            s = i;
+            break;
+        }
+    }
+    document.getElementById("horn-sound").setAttribute('src',sounds[s]);
     let audio = document.getElementById("horn-sound");
-    audio.play();
+    audio.play()
+    event.preventDefault();
 });
+
+
 // TODO
