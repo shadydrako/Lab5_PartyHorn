8af703cb40e7bd734ea1c8ca75b5c84128e230ab
Ravi <rtapia@ucsd.edu>
2021-02-02 19:11:43 -0800

    testing on click event

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,12 @@
-// main.js
-hello
+ //main.js
+
+ var btn = document.querySelector('button');
+ btn.onclick = playSound();
+ 
+function playSound(){
+    alert( "The button was pressed");
+}
+
+
+ 
 // TODO
