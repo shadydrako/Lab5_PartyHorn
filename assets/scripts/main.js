ee2d1b6323157088460def1b5ac00205912df55d
Ravi <rtapia@ucsd.edu>
2021-02-03 12:53:04 -0800

    working slider and number input

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,31 @@
  //main.js
 
 let sounds = ["./assets/media/audio/air-horn.mp3","./assets/media/audio/car-horn.mp3","./assets/media/audio/party-horn.mp3"];
+let images = ["./assets/media/icons/volume-level-0.svg", "./assets/media/icons/volume-level-1.svg","./assets/media/icons/volume-level-2.svg", "./assets/media/icons/volume-level-3.svg"];
+let lvl = 1;
+let nosound = false;
+/* 
+*   When the slider value changes the textual indicator also changes.
+*   When the textual indicator changes, the visual should also change.
+*/
+
+document.getElementById("volume-number").addEventListener("input",function(){
+    var val = document.getElementById("volume-number").value;
+    if(val == 0){
+        nosound = true;
+    }
+    lvl = val/100;
+    document.getElementById("volume-slider").value = val;
+});
+
+document.getElementById("volume-slider").addEventListener("input",function(){
+    var val = document.getElementById("volume-slider").value;
+    if(val == 0){
+        nosound = true;
+    }
+    document.getElementById("volume-number").value = val; 
+});
+
 
 //add event listener to each button to change picture
 document.getElementById("radio-air-horn").addEventListener("change",function(){
@@ -22,7 +47,9 @@ document.getElementById("radio-party-horn").addEventListener("change",function()
 
 document.querySelector('button').addEventListener('click',function(){
     let audio = document.getElementById("horn-sound");
-    audio.play()
+    audio.volume = lvl;
+    audio.mute = nosound;
+    audio.play();
     event.preventDefault();
 });
 
