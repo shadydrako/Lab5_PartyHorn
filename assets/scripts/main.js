0c68d0722e7315d12bafc5807bed8f3a5151d587
Ravi <rtapia@ucsd.edu>
2021-02-02 19:20:45 -0800

    added event listener

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,14 @@
  //main.js
 
- var btn = document.querySelector('button');
- btn.onclick = playSound();
  
 function playSound(){
     alert( "The button was pressed");
+    var audioElement = new Audio('assets\media\audio\air-horn.mp3');
+    audioElement.muted=false;
+    audioElement.play();
 }
+
+var btn = document.querySelector('button').addEventListener("click",playSound());
 
 
  
