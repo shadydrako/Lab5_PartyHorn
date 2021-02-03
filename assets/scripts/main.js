da4c764673b72b3caffc3fc01c6ede8e29d8d227
Ravi <rtapia@ucsd.edu>
2021-02-03 10:44:08 -0800

    design/added functional changing pictures w/ soun

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,17 +2,25 @@
 
 let sounds = ["./assets/media/audio/air-horn.mp3","./assets/media/audio/car-horn.mp3","./assets/media/audio/party-horn.mp3"];
 
+//add event listener to each button to change picture
+document.getElementById("radio-air-horn").addEventListener("change",function(){
+    document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    document.getElementById("horn-sound").setAttribute('src',sounds[0]);
+});
+
+document.getElementById("radio-car-horn").addEventListener("change",function(){
+    document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    document.getElementById("horn-sound").setAttribute('src',sounds[1]);
+});
+
+document.getElementById("radio-party-horn").addEventListener("change",function(){
+    document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    document.getElementById("horn-sound").setAttribute('src',sounds[2]);
+});
+
+
+
 document.querySelector('button').addEventListener('click',function(){
-    let s = 0;
-    var form = document.querySelector("form");
-    var radios = document.getElementsByName("radio-sound");
-    for(let i = 0, length = radios.length; i < length; i++){
-        if(radios[i].checked){
-            s = i;
-            break;
-        }
-    }
-    document.getElementById("horn-sound").setAttribute('src',sounds[s]);
     let audio = document.getElementById("horn-sound");
     audio.play()
     event.preventDefault();
